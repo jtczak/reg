b45923e0cad5cce88302cefab66901538d5a3bdc
Paulina <idczak.paulina@gmail.com>
2021-07-01 17:19:47 +0200

    show and clear error

--- script.js
+++ script.js
@@ -7,6 +7,45 @@ const sendBtn = document.querySelector('.send');
 const popup = document.querySelector('.popup');
 const closePopup = document.querySelector('.close');
 
+
+const showError = (input, msg) => {
+    //INPUT -> inputs
+    //MSG -> placeholders
+    const formBox = input.parentElement;
+    const errorMsg = formBox.querySelector('.error-text');
+
+    formBox.classList.add('error');
+    errorMsg.textContent = msg;
+};
+
+const clearError = input => {
+    const formBox = input.parentElement;
+    formBox.classList.remove('error');
+
+}
+
+const checkForm = input => {
+    input.forEach(el => {
+
+        if (el.value === '') {
+
+            showError(el, el.placeholder);
+        } else {
+            console.log('ok');
+        }
+    })
+};
+
+// INPUT from checkForm -> array with inputs
+// EL is every variable from array
+
+sendBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    checkForm([username, pass, pass2, email]);
+})
+
+
 clearBtn.addEventListener('click', e => {
     e.preventDefault();
 
